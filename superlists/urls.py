a596937498f32652e7febd62cdce18ed5db80243
John Alcher M. Doliras <johnalcherdoloiras@gmail.com>
2020-05-17 10:40:16 +0800

    Put the URL mappings for the lists app on its own urls.py

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from lists import views
+from django.urls import path, include
 
 urlpatterns = [
-	path('', views.home_page)
+	path('', include('lists.urls'))
 ]
