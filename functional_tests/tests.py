af5eb9f7e81f3feb843440db206eb00460b8addd
John Alcher M. Doliras <johnalcherdoloiras@gmail.com>
2020-05-18 06:53:48 +0800

    Use polling code instead of explicit time.sleep calls

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
@@ -11,10 +14,20 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_table(self, text, table_id):
-		table = self.browser.find_element_by_id(table_id)
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn(text, [row.text for row in rows])
+	def wait_for_row_in_table(self, text, table_id):
+		start_time = time.time()
+		while True:
+			try:
+				table = self.browser.find_element_by_id(table_id)
+				rows = table.find_elements_by_tag_name('tr')
+				self.assertIn(text, [row.text for row in rows])
+
+				return
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+		
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -39,19 +52,17 @@ class NewVisitorTest(LiveServerTestCase):
 		# When she hits enter, the page updates, and now the page lists
 		# "1. Buy peacock feathers"
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
-		self.check_for_row_in_table('1. Buy peacock feathers', 'todo_table')
+		self.wait_for_row_in_table('1. Buy peacock feathers', 'todo_table')
 
 		# There is still a box inviting her to add another item. She enters
 		# "Use peacock feathers to make a fly"
 		inputbox = self.browser.find_element_by_id('new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(1)
 
 		# The page updates again, and now both items are on the list
-		self.check_for_row_in_table('1. Buy peacock feathers', 'todo_table')
-		self.check_for_row_in_table('2. Use peacock feathers to make a fly', 'todo_table')
+		self.wait_for_row_in_table('1. Buy peacock feathers', 'todo_table')
+		self.wait_for_row_in_table('2. Use peacock feathers to make a fly', 'todo_table')
 
 		self.fail('Finish the test!')
 
