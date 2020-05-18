72292f5585b520154de5144081b7cd633d8d7974
John Alcher M. Doliras <johnalcherdoloiras@gmail.com>
2020-05-18 08:06:41 +0800

    Implement List model with Item relation

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from .models import Item
+from .models import Item, List
 
 def home_page(request):
 	return render(request, 'lists/home.html')
@@ -10,5 +10,6 @@ def view_list(request):
 	return render(request, 'lists/list.html', {'items': Item.objects.all()})
 
 def new_list(request):
-	item = Item.objects.create(text=request.POST['body'])
+	list_ = List.objects.create()
+	item = Item.objects.create(text=request.POST['body'], list=list_)
 	return redirect('/lists/the-only-list/')
